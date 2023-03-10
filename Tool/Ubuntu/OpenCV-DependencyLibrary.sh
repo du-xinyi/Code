2d1d70a7873b03a7aea8fc3339909fea2ded0b32
du-xinyi <386476500@qq.com>
2023-03-10 16:15:00 +0800

    First Commit

---
+++ Tool/Ubuntu/OpenCV-DependencyLibrary.sh
@@ -0,0 +1,39 @@
+# 编译工具及构建工具
+sudo apt-get install -y git gcc g++ cmake cmake-gui make build-essential aptitude pkg-config ffmpeg
+# 图形界面和窗口管理器
+sudo apt-get install -y libgtk-3-dev libcanberra-gtk-module libcanberra-gtk3-module
+# 音视频处理
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libavutil-dev libxvidcore-dev libx264-dev 
+sudo apt-get install -y libxine2-dev libavresample-dev
+# 流媒体处理
+sudo apt-get install -y libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev
+# 图像格式处理
+sudo add-apt-repository "deb http://security.ubuntu.com/ubuntu xenial-security main"
+sudo apt-get update
+sudo apt-get install -y libjasper1 libjasper-dev
+sudo apt-get install -y libjpeg-dev libpng-dev libtiff-dev libwebp-dev libopenexr-dev
+# 相机捕捉和处理
+sudo apt-get install -y libv4l-dev libdc1394-22-dev libgphoto2-dev
+# 数学计算和矩阵操作
+sudo apt-get install -y libatlas-base-dev libcblas-dev liblapack-dev liblapacke-dev libblas-dev libopenblas-dev 
+sudo apt-get install -y libsuitesparse-dev libgtest-dev libgoogle-glog-dev libeigen3-dev libceres-dev 
+# 命令行参数处理
+sudo apt-get install -y libgflags-dev
+# 科学数据存储和交换
+sudo apt-get install -y libhdf5-dev
+# 序列化和反序列化
+sudo apt-get install -y libprotobuf-dev
+# Qt 支持
+sudo apt-get install -y qt5-default qtcreator
+# Python 支持
+sudo apt-get install -y python3-dev python3-numpy python3-pip python3-pyqt5
+# 3D 处理
+sudo apt-get install -y libvtk6-dev libgdal-dev libglew-dev
+# TBB 并行化框架
+sudo apt-get install -y libtbb-dev
+# LAPACK 库
+sudo apt-get install -y liblapacke-dev
+# Boost 库
+sudo apt-get install -y libboost-all-dev
+# 字体渲染
+sudo apt-get install -y libfreetype6-dev
