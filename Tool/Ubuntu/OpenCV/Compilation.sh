dca4be561e858ec8b7d4e10ed2830afba3d89f87
du-xinyi <386476500@qq.com>
2023-03-10 21:04:02 +0800

    First Commit

---
+++ Tool/Ubuntu/OpenCV/Compilation.sh
@@ -0,0 +1,24 @@
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+      -D CMAKE_INSTALL_PREFIX=/usr/local \
+      -D OPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules \
+      -D OPENCV_ENABLE_NONFREE=ON \
+      -D OPENCV_GENERATE_PKGCONFIG=ON \
+      -D WITH_CUDA=ON \
+      -D ENABLE_FAST_MATH=ON \
+      -D CUDA_FAST_MATH=ON \
+      -D WITH_CUBLAS=ON \
+      -D WITH_INF_ENGINE=ON \
+      -D WITH_NGRAPH=ON \
+      -D WITH_IPP=ON \
+      -D WITH_TBB=ON \
+      -D WITH_OPENMP=ON \
+      -D WITH_OPENGL=ON \
+      -D WITH_PTHREADS_PF=ON \
+      -D WITH_QT=ON \
+      -D WITH_V4L=ON \
+      -D WITH_VTK=ON \
+      -D WITH_OPENGL=ON \
+      -D BUILD_JAVA=OFF \
+      -D BUILD_opencv_python3=OFF ..
+make -j$(nproc)
+sudo make install
