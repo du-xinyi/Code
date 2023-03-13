dc4d5bfe373ecf73a10df9ec6a59d55d0bf9bacc
du-xinyi <386476500@qq.com>
2023-03-13 20:48:38 +0800

    Update Tool/DeepLearning/ChangeSuffix

--- Tool/DeepLearning/ChangeSuffix/main.py
+++ Tool/DeepLearning/ChangeSuffix/main.py
@@ -1,11 +1,11 @@
 import os
 
-os.chdir(r'./datasets/images') # 需要修改后缀的文件目录
+os.chdir(r'./img') # 需要修改后缀的文件目录
 
 files = os.listdir('./') # 列出当前目录下所有的文件
 for fileName in files:
     print(fileName)
     portion = os.path.splitext(fileName)
-    newName = portion[0] + ".jpg" # 修改为目标后缀
+    newName = portion[0] + ".png" # 修改为目标后缀
     os.rename(fileName, newName)
     print("Change Suffix Right")
